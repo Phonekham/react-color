a0ddf8da0e13fb0a6770a5ad7b7a59d2ffc4d30b
Phonekham <phone.madridista@gmail.com>
2019-09-20 14:22:35 +0700

    setup

---
+++ src/Palatte.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Palatte extends Component {
+  render() {
+    return (
+      <div className="pallette">
+        <div className="pallett-colors">pallette</div>
+      </div>
+    );
+  }
+}
+
+export default Palatte;
