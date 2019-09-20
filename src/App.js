df6f1de6cd25f06946e2316c767738ea2fa743e1
Phonekham <phone.madridista@gmail.com>
2019-09-20 16:51:16 +0700

    Generating Shades of Colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Pallette from "./Palatte";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div className="App">
-      <Pallette {...seedColors[4]}></Pallette>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+
+    return (
+      <div className="App">
+        <Pallette {...seedColors[4]}></Pallette>
+      </div>
+    );
+  }
 }
 
 export default App;
