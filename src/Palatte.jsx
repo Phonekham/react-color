d72e7014dabee7b35dffc04d797c1b073ff2594e
Phonekham <phone.madridista@gmail.com>
2019-09-21 14:12:45 +0700

    Select Color Format

--- src/Palatte.jsx
+++ src/Palatte.jsx
@@ -7,22 +7,30 @@ import Navbar from "./Navbar";
 class Palatte extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
   }
 
   changeLevel = level => {
     this.setState({ level });
     console.log(level);
   };
+
+  changeFormat = val => {
+    this.setState({ format: val });
+  };
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxs = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name}></ColorBox>
+      <ColorBox background={color[format]} name={color.name}></ColorBox>
     ));
     return (
       <div className="pallette">
-        <Navbar level={level} changeLevel={this.changeLevel}></Navbar>
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        ></Navbar>
         <div className="pallett-colors">{colorBoxs}</div>
       </div>
     );
