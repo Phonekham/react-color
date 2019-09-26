6b1e9ac2c98d12da07bb140b8cb2d85abb1fbe64
Phonekham <phone.madridista@gmail.com>
2019-09-26 14:57:42 +0700

    Adding NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
 class App extends React.Component {
   findPalette(id) {
@@ -16,6 +17,11 @@ class App extends React.Component {
     // console.log(generatePalette(seedColors[4]));
     return (
       <Switch>
+        <Route
+          exact
+          path="/palette/new"
+          render={() => <NewPaletteForm></NewPaletteForm>}
+        ></Route>
         <Route
           exact
           path="/"
