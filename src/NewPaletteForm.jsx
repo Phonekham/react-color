4c053272ee2615ffb929c66b45fc81e2f03602de
Phonekham <phone.madridista@gmail.com>
2019-09-30 16:13:17 +0700

    Creating Draggable Color Box

--- src/NewPaletteForm.jsx
+++ src/NewPaletteForm.jsx
@@ -15,6 +15,8 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import Button from "@material-ui/core/Button";
 
+import DraggableColorBox from "./DraggableColorBox";
+
 const drawerWidth = 400;
 
 const styles = makeStyles(theme => ({
@@ -57,6 +59,7 @@ const styles = makeStyles(theme => ({
   },
   content: {
     flexGrow: 1,
+    height: "calc(100vh - 64px)",
     padding: theme.spacing(3),
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
@@ -168,11 +171,9 @@ function NewPaletteForm() {
         })}
       >
         <div className={classes.drawerHeader} />
-        <ul>
-          {colors.map(color => (
-            <li style={{ backgroundColor: color }}>{color}</li>
-          ))}
-        </ul>
+        {colors.map(color => (
+          <DraggableColorBox color={color} key={color}></DraggableColorBox>
+        ))}
       </main>
     </div>
   );
